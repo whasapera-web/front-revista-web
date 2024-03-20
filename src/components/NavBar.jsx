aefc0cab1d59ac857221ca88fe27260ac1459b42
Fontana Roman <desarrolladorautodidacta@gmail.com>
2024-03-20 18:24:17 -0300

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,11 +7,11 @@ export default function NavBar() {
     };
     return (
         <>
-            <header className="xl:flex xl:justify-evenly xl:items-center xl:w-full">
+            <header className="xl:flex xl:justify-evenly xl:items-center xl:w-full le:mr-2 le:w-content">
                 <div className="flex justify-around items-center xl:flex xl:flex-row xl:justify-around xl:items-center xl:w-3/6">
                     <img src="/logo/logo-navbar.png" alt="imagen no soportada" className="w-20 md:w-24 xl:w-20" onClick={redirectHome}/>
                     <nav className="flex justify-center w-max items-center flex-wrap bg-nvPink md:justify-center md:w-max md:p-1 xl:flex-nowrap xl:w-max xl:h-10 xl:flex xl:flex-row xl:justify-center xl:items-center">
-                        <Link to={"/nosotros"} className="border border-r border-l border-black w-20 h-7 text-xs flex justify-center items-center"> NOSOTROS </Link>
+                        <Link to={"/nosotros"} className="border border-r border-l border-black w-20 le:w-22 h-7 text-xs flex justify-center items-center"> NOSOTROS </Link>
                         <Link to={"/comercios"} className="border  border-r border-l border-black w-20 h-7 text-xs flex justify-center items-center"> COMERCIOS </Link>
                         <Link to={"/profecionales-de-bellesa"} className="border  border-r border-l border-black w-20 h-7 text-xs flex justify-center items-center"> PROF.BELLESAS </Link>
                         <Link to={"/profecionales-de-salud"} className="border  border-r border-l border-black w-20 h-7 text-xs flex justify-center items-center"> PROF.SALUD </Link>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,8 @@ export default function Home() {
         <>
             <main className="xl:w-full xl:h-max xl:flex xl:justify-center xl:items-center xl:flex-col">
                 <NavBar />
-                <section className="backgroud xl:w-3/6 xl:h-screen p-2 md:p-16 xl:p-2">
-                    <article className="relative w-full h-max flex flex-row p-4 justify-center items-start">
+                <section className="relative xl:w-3/6 xl:h-screen p-2 md:p-16 xl:p-2">
+                    {/* <article className="relative w-full h-max flex flex-row p-4 justify-center items-start">
                         <img src="/logo/muñeca.png" alt="logo-revista-web" className="w-28 md:w-40 xl:w-36" />
                         <div className="flex flex-col justify-center items-center gap-20 mt-2">
                             <div>
@@ -23,34 +23,30 @@ export default function Home() {
                                 <h2 className="text-center stroke-white text-base scale-125 md:text-4xl xl:text-4xl rotate-00">PUBLICIDADES <br /> NICOLEÑAS Y DE LA ZONA</h2>
                             </div>
                         </div>
-                    </article>
-                    <article className="relative w-full h-max flex flex-row p-4 justify-center items-start flex-wrap gap-12 xl:mt-8">
-                        <div className="w-full flex flex-col justify-start items-start gap-4">
+                    </article> */}
+                    <article className="absolute backgroud h-2vh w-full flex flex-row p-4 justify-center items-end gap-12 xl:mt-8">
+                        <div className="w-full flex flex-col justify-start items-start gap-2 mb-10 le:mb-5">
                             <div className="flex gap-1 items-center justify-center">
                                 <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-green-500"> </div>
-                                <a href="https://www.facebook.com/profile.php?id=100063715656346" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl md:text-2xl text-black xl:text-xl">FARMACIAS DE TURNO</a>
+                                <a href="https://www.facebook.com/profile.php?id=100063715656346" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl md:text-2xl text-black xl:text-xl">FARMACIAS</a>
                             </div>
                             <div className="flex gap-1 items-center justify-center">
                                 <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-blue-400"> </div>
-                                <a href="https://www.e-evhsa.com.ar/Horarios" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">HORARIOS COLECTIVOS</a>
+                                <a href="https://www.e-evhsa.com.ar/Horarios" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">COLECTIVOS</a>
                             </div>
                             <div className="flex gap-1 items-center justify-center">
                                 <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-orange-300"> </div>
                                 <a href="https://www.sannicolasciudad.gob.ar/noticias" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">NOTICIAS</a>
                             </div>
                             <div className="flex items-center justify-center gap-1">
-                                <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-red-600"> </div>
-                                <a href="https://www.sannicolasciudad.gob.ar/" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">SN Trámites Municipales</a>
+                                <div className="h-4 w-6 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-red-600 mb-8"> </div>
+                                <a href="https://www.sannicolasciudad.gob.ar/" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">TRÁMITES MUNICIPALES</a>
                             </div>
                         </div>
-                        <div className="w-full flex flex-col justify-center items-start gap-4">
+                        <div className="w-full flex flex-col justify-center items-start gap-2 mb-10">
                             <div className="flex gap-1 items-center justify-center">
                                 <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-red-500"> </div>
                                 <a href="https://www.clarin.com/loterias-y-quinielas" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">LOTERIAS</a>
-                            </div>
-                            <div className="flex gap-1 items-center justify-center">
-                                <div className="h-4 w-4 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-pink-500"> </div>
-                                <a href="https://www.argentina.gob.ar/servicio/solicitar-turnos-en-anses" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">TURNOS ANSES</a>
                             </div>
                             <div className="flex items-center gap-1 justify-center">
                                 <div className="h-4 w-4 border-2 md:h-10 xl:w-6 xl:h-6 md:w-10 rounded-full bg-yellow-300"> </div>
@@ -59,6 +55,10 @@ export default function Home() {
                             <div className="flex items-center gap-1 justify-center">
                                 <div className="h-4 w-4 border-2 md:h-10 xl:w-6 xl:h-6 md:w-10 rounded-full bg-blue-600"> </div>
                                 <a href="https://www.accuweather.com/es/ar/san-nicol%C3%A1s-de-los-arroyos/2917/hourly-weather-forecast/2917" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">CLIMA</a>
+                            </div>
+                            <div className="flex gap-1 items-center justify-center">
+                                <div className="h-4 w-6 le:w-4 le:mb-0 md:h-10 md:w-10 xl:w-6 xl:h-6 border-2 rounded-full bg-pink-500 mb-8"> </div>
+                                <a href="https://www.argentina.gob.ar/servicio/solicitar-turnos-en-anses" target="_blank" rel="noopener noreferrer" className="stroke-black text-xl text-black md:text-2xl xl:text-xl">TURNOS ANSES</a>
                             </div>
                         </div>
                     </article>
